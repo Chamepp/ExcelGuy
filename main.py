b1a329ecdf6a469b6a132df7efcf0147d5e01a19
root <root@Kali>
2020-09-12 18:59:38 -0500

    completed excel convertor & improved menu

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ My First Python Script
 Developed With LOVE By Ashkan Ebtekari
 '''
 
-# Main Local Variables
 
+#imports
+
+from win32com import client
+import win32api
+
+
+
+# Main Local Variables
 
 Introduction = "Welcome To Ashkans Script " \
                "\n You Can Easily Handle Your Excel WorkSheets " \
@@ -14,7 +21,7 @@ Introduction = "Welcome To Ashkans Script " \
 
 Menu = "1- Excel Merger " \
        "\n 2- Excel Scraper" \
-       "\n 3- Excel Checker" \
+       "\n 3- Excel Converter" \
        "\n \n \n"
 
 # Print Instructions
@@ -32,6 +39,10 @@ Menu_Merger = UserInput == "1"
 Menu_Scraper = UserInput == "2"
 Menu_Checker = UserInput == "3"
 
+
+
+# Excel Converter
+
 # Statements
 
 if Menu_Merger:
@@ -44,7 +55,47 @@ elif Menu_Scraper:
 
 elif Menu_Checker:
 
-    print("Selected Excel Checker")  # Excel Checker
+    # Instruction
+    print("Selected Excel Converter")
+
+    # Excel File Collector
+    Excel_Source_Input = input("Give Me The Excel Source : ")
+    Excel_Source = Excel_Source_Input
+    Excel_Name_Input = input("Excel Name : ")
+    Excel_Name = Excel_Name_Input
+    Excel_Final_Output = r'{}{}'.format(Excel_Source , Excel_Name)
+    print("Excel Source Collected Successfully ... \n \n")
+
+    # PDF File Collector
+    PDF_Source_Input = input("PDF OutPut File Directory : ")
+    PDF_Source = PDF_Source_Input
+    PDF_Name_Input = input("PDF Name : ")
+    PDF_Name = PDF_Name_Input
+    PDF_Final_Output = r'{}{}'.format(PDF_Source, PDF_Name)
+    print("PDF Info Collected Successfully ... \n \n")
+
+
+    # Excel Application Warmups
+    Application = client.DispatchEx("Excel.Application")
+    Application.Interactive = False
+    Application.Visible = False
+    Student = Application.Workbooks.Open(Excel_Final_Output)
+
+    try:
+
+        Student.ActiveSheet.ExportAsFixedFormat(0, PDF_Final_Output)
+
+    except Exception as exc:
+
+        print("Convertation Process Failed")
+        print(exc)
+
+    finally:
+
+        Student.Close()
+        Application.Exit()
+        print("PDF FILE GENERATED SUCCESSFULLY !!!")
+
 
 
 else:
